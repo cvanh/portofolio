d9828d7766ad7bba00f3dcba066c28018325a524
constantijn <constantijn@vanhartesveldt.nl>
2022-01-21 00:45:25 +0100

    work

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # my shitty portofolio
 if you have any recomondations please send me a mail with feedback. or open a issue.
+
+
+`php8.1 -S localhost:8000 /etc/php/8.1/apache2/php.ini`
--- backend/src/index.php
+++ backend/src/index.php
@@ -45,15 +45,16 @@ $router->post('/contact/insert', function () use ($database) {
 
 // creates a blog
 $router->post('/crud/createblog', function () use ($database) {
-    $query = `
+    $query = "
     INSERT INTO contacts (id, name, email, content,date) VALUES (NULL, '{$_POST["name"]}', '{$_POST["email"]}', '{$_POST["content"]}', NOW()); 
-    `;
+    ";
     $database->query($query);
     echo "succes";
 });
 
 // test api end point https://www.php.net/manual/en/book.apcu.php
 $router->get('/testing', function () use ($database) {
+    echo "c";
     $apc_key = "{$_SERVER['SERVER_NAME']}~login:{$_SERVER['REMOTE_ADDR']}";
     $apc_blocked_key = "{$_SERVER['SERVER_NAME']}~login-blocked:{$_SERVER['REMOTE_ADDR']}";
 
@@ -64,17 +65,17 @@ $router->get('/testing', function () use ($database) {
         exit();
     }
 
-    $success = login($_POST['username'], $_POST['password']);
+    $success = false;
     if (!$success) {
         $blocked = (int)apc_fetch($apc_blocked_key);
-
+        echo "a";
         apc_store($apc_key, $tries + 1, pow(2, $blocked + 1) * 60);  # store tries for 2^(x+1) minutes: 2, 4, 8, 16, ...
         apc_store($apc_blocked_key, $blocked + 1, 86400);  # store number of times blocked for 24 hours
     } else {
         apc_delete($apc_key);
         apc_delete($apc_blocked_key);
+        echo "b";
     }
 });
-
 
 $router->run();
