095091f710672d3e83646b6bc374d25d4de2f383
constantijn <constantijn@vanhartesveldt.nl>
2022-01-28 13:14:45 +0100

    work

--- frontend/pages/admin/createblog/index.js
+++ frontend/pages/admin/createblog/index.js
@@ -2,18 +2,18 @@ import Admin from "../../../styled/admin.styled";
 const config = require("../../../config.json")
 
 function CreateBlog(){
-    return(
-        <div>
-            <h1>create a blog post</h1>
+    // return(
+    //     <div>
+    //         <h1>create a blog post</h1>
 
-            <Admin.Form method="post" action={`${config.BaseUrl}/crud/createblog`}>
-                <input type="text" placeholder="title" name="title"></input>
-                <input type="file" placeholder="file" name="file"></input>
-                <input type="text" placeholder="body" name="body"></input>
-                <input type="submit"></input>
-            </Admin.Form>
-        </div>
-    )
+    //         <Admin.Form method="post" action={`${config.BaseUrl}/crud/createblog`}>
+    //             <input type="text" placeholder="title" name="title"></input>
+    //             <input type="file" placeholder="file" name="file"></input>
+    //             <input type="text" placeholder="body" name="body"></input>
+    //             <input type="submit"></input>
+    //         </Admin.Form>
+    //     </div>
+    // )
 }
 
 export default CreateBlog;
