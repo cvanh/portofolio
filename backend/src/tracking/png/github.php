f8e65ddd3bdf2874813b44d083649188ec59656b
constantijn <constantijn@vanhartesveldt.nl>
2022-01-28 08:31:52 +0100

    added last seen option

--- backend/src/tracking/png/github.php
+++ backend/src/tracking/png/github.php
@@ -2,71 +2,73 @@
 require '../../../vendor/autoload.php';
 require_once("../../../lib/db.php");
 require_once("../../functions/format_query.php");
+require_once("./functions/get_last_seen.php");
+require_once("./functions/report_page_serve.php");
 
 // load the .env
 $dotenv = Dotenv\Dotenv::createImmutable("../../../");
 $dotenv->load();
 
+$font_file = './terminal.TTF';
+
 // create the database connection
 $database = new Database($_ENV["MYSQL_HOST"], $_ENV["MYSQL_USERNAME"], $_ENV["MYSQL_PASSWORD"], $_ENV["MYSQL_DATABASE"]);
 
-$database->query("INSERT INTO `tracking` (`id`, `ip`, `last_seen`, `last_agent`,`track_id`) VALUES (NULL, '{$_SERVER["REMOTE_ADDR"]}', NOW(), '{$_SERVER["HTTP_USER_AGENT"]}','{$_GET["code"]}');");
+report_last_seen($database);
+$last_seen = get_last_seen($database);
 
-$last_seen = $database->format_query("SELECT * FROM `tracking` WHERE ip = "{$_SERVER['REMOTE_ADDR']}" ORDER BY last_seen DESC; ");
-var_dump($last_seen);   
+// create the blank image canvas
+$im = imagecreatetruecolor(3000, 450);
 
-// // create the blank image canvas
-// $im = imagecreatetruecolor(800, 450);
+// create the line for user's ip
+imagefttext(
+    $im,
+    50,
+    0,
+    50,
+    100,
+    imagecolorallocate($im, 0, 150, 0),
+    $font_file,
+    "ip: {$_SERVER["REMOTE_ADDR"]}"
+);
 
-// // create the line for user's ip
-// imagefttext(
-//     $im,
-//     50,
-//     0,
-//     50,
-//     100,
-//     imagecolorallocate($im, 0, 150, 0),
-//     './terminal.TTF',
-//     "ip: {$_SERVER["REMOTE_ADDR"]}"
-// );
+// creates the last seen
+imagefttext(
+    $im,
+    50,
+    0,
+    50,
+    200,
+    imagecolorallocate($im, 0, 150, 0),
+    $font_file,
+    "last seen: {$last_seen}"
+);
 
-// // creates the last seen
-// imagefttext(
-//     $im,
-//     50,
-//     0,
-//     50,
-//     200,
-//     imagecolorallocate($im, 0, 150, 0),
-//     './terminal.TTF',
-//     "last seen: 24-1-2021"
-// );
+// creates the line for the user-agent
+imagefttext(
+    $im,
+    50,
+    0,
+    50,
+    300,
+    imagecolorallocate($im, 0, 150, 0),
+    $font_file,
+    "user-agent: {$_SERVER["HTTP_USER_AGENT"]}"
+);
 
-// // creates the line for the user-agent
-// imagefttext(
-//     $im,
-//     50,
-//     0,
-//     50,
-//     300,
-//     imagecolorallocate($im, 0, 150, 0),
-//     './terminal.TTF',
-//     "user-agent: {$_SERVER["HTTP_USER_AGENT"]}"
-// );
-
-// // creates the line for the code
-// imagefttext(
-//     $im,
-//     50,
-//     0,
-//     50,
-//     400,
-//     imagecolorallocate($im, 0, 150, 0),
-//     './terminal.TTF',
-//     "code?: {$_GET["code"]}"
-// );
+// creates the line for the tracking code
+imagefttext(
+    $im,
+    50,
+    0,
+    50,
+    400,
+    imagecolorallocate($im, 0, 150, 0),
+    $font_file,
+    "code?: {$_GET["code"]}"
+);
 
 
-// header('Content-Type: image/png');
-// imagepng($im);
-// imagedestroy($im);
+header('Content-Type: image/png');
+imagepng($im);
+imagedestroy($im);
