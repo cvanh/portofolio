47ef5d4f6511a67c112a9b62def81b2777054b71
constantijn <constantijn@vanhartesveldt.nl>
2022-02-01 00:19:06 +0100

    working on last viewed ip's

--- backend/src/tracking/png/functions/get_last_visits.php
+++ backend/src/tracking/png/functions/get_last_visits.php
@@ -1,21 +1,10 @@
 <?php
 function get_last_visits($data){
         // this works but generates a lot of errors when $data count is lower than 6
-        // for ($i=0; $i < 6; $i++) { 
-        //     $last_visits = $last_visits . $data[$i]["ip"] . ", ";
-        // }
+        $last_visits = null;
+        for ($i=0; $i < (6 ^ count($data)); $i++) { 
+            $last_visits = $last_visits . $data[$i]["ip"] . ", ";
+        }
     
-        // return $last_visits;
-
-        // complete shite ples fix me
-        $last_visits = null; 
-
-        $last_visits = $last_visits . $data[1]["ip"] . ", ";
-        $last_visits = $last_visits . $data[2]["ip"] . ", ";
-        $last_visits = $last_visits . $data[3]["ip"] . ", ";
-        $last_visits = $last_visits . $data[4]["ip"] . ", ";
-        $last_visits = $last_visits . $data[5]["ip"] . ", ";
-        $last_visits = $last_visits . $data[6]["ip"] . ", ";
-
         return $last_visits;
 }
