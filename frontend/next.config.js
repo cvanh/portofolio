70159f178455fb26f91b8f713bd92df418efcaab
constantijn <constantijn@vanhartesveldt.nl>
2022-02-06 16:27:07 +0100

    updated url in config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,6 @@
-// https://stackoverflow.com/questions/66896774/how-to-add-domains-to-next-config-js-for-next-image-while-using-a-plugin
-const withImages = require("next-images");
-
-module.exports = withImages({
+module.exports = {
+    reactStrictMode: true,
     images: {
-        domains: ['imaretarded.dev']
-    },
-    webpack(config, options) {
-        return config;
+      domains: ["imaretarded.dev"],
     }
-});
+  }
